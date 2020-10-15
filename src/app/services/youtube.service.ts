fd56afcf4452dd4d8293eaa3636298a12f6fb843
Luism20 <luisjuares73@gmail.com>
2020-10-15 11:01:59 -0500

    LM20

---
+++ src/app/services/youtube.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { YoutubeResponse } from '../models/youtube.models';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class YoutubeService {
+
+  private youtubeUrl = 'https://www.googleapis.com/youtube/v3';
+  private apikey = 'AIzaSyDsjeMwBxmimoG8XdpAEexzAp45hg02WFg';
+  private playlist = 'd1ujbNq87BQWIllf98ZHNDgxXDo';
+  private nextPageToken = '';
+
+  constructor(private http: HttpClient) {}
+
+  getVideos() {
+
+    const url = `${ this.youtubeUrl }/playlistItems`;
+
+    const params = new HttpParams()
+    .set('part', 'snippet')
+    .set('maxResults', '10')
+    .set('playlistId', this.playlist)
+    .set('key', this.apikey)
+    .set('pageToken', this.nextPageToken);
+
+    return this.http.get<YoutubeResponse>( url, { params })
+    .pipe(
+
+      map( resp => {
+        this.nextPageToken = resp.nextPageToken;
+        return resp.items;
+      }),
+
+      map( items => items.map( video => video.snippet ) )
+
+      );
+  }
+
+}
